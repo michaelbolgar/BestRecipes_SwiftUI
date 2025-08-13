a3c721b61b7f7727426cb5eee71d7783d5620f9b
Dimitriy <121857982+SnakCat@users.noreply.github.com>
2025-08-13 10:36:06 +0300

    Trushin/UI saved recipes (#10)
    * created saved recipes view
    
    * refactor saved recipes view

---
+++ BestRecipes/Entities/RatingView.swift
@@ -0,0 +1,27 @@
+import SwiftUI
+
+struct RatingView: View {
+    var rating: Double
+
+    var body: some View {
+        ZStack {
+            RoundedRectangle(cornerRadius: 8)
+                .fill(Color(uiColor: AppColor.backgrountRatingView))
+                .frame(width: 58, height: 27)
+                .padding(8)
+            HStack {
+                Image(systemName: "star.fill")
+                    .resizable()
+                    .foregroundStyle(.black)
+                    .frame(width: 12, height: 12)
+                Text(String(format: "%.1f", rating))
+                    .font(.custom(AppFont.bold, size: 14))
+                    .foregroundStyle(.white)
+            }
+        }
+    }
+}
+
+#Preview {
+    RatingView(rating: 5.0)
+}
