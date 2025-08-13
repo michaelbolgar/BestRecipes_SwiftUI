743b16208a4af94a71a46282198b82bd0efbf9b9
KellerDmitriy <dmit.keller@gmail.com>
2025-08-13 10:37:48 +0300

    Keller/task#3 (#11)
    * add initial HomeUI
    
    * add colorSet
    
    * fix colorSet
    
    * add SearchRecipeCell

--- BestRecipes/Design_System/Fonts/AppFont.swift
+++ BestRecipes/Design_System/Fonts/AppFont.swift
@@ -1,7 +1,31 @@
 import Foundation
+import SwiftUI
 
 struct AppFont {
     static let bold: String = "Poppins-Bold"
     static let semibold: String = "Poppins-SemiBold"
     static let regular: String = "Poppins-Regular"
+}
+
+// MARK: - Font Extensions for BestRecipesApp
+extension Font {
+    static let recipesNavTitle = Font.custom(AppFont.bold, size: 24)
+    static let placeholderText = Font.custom(AppFont.regular, size: 14)
+}
+
+// MARK: - Text Extensions with Font Weights
+extension Text {
+    
+    func recipesNavTitleStyle() -> some View {
+        self
+            .font(.recipesNavTitle)
+            .foregroundStyle(.neutral100)
+    }
+    
+    func recipesPlaceholderStyle() -> some View {
+        self
+            .font(.placeholderText)
+            .foregroundStyle(.neutral30)
+    }
+    
 }
