1e57ca2c2b5067269f82ae6f857698965be40869
Alex <97949097+abayellow@users.noreply.github.com>
2025-08-14 19:26:15 +1100

    Abanshin/network service (#17)
    * Modife network router.
    
    * Modify recipe model. Remove optional values.
    
    * Create mealType.
    
    * Create cuisine.
    
    * Modify endpoint. Change string parametr to enum. Add cuisine type param.
    
    ---------
    
    Co-authored-by: Michael Bolgar <119865051+michaelbolgar@users.noreply.github.com>

---
+++ BestRecipes/Models/MealType.swift
@@ -0,0 +1,25 @@
+//
+//  MealType.swift
+//  BestRecipes
+//
+//  Created by Alexander Abanshin on 14.08.2025.
+//
+
+import Foundation
+
+enum MealType: String {
+    case mainCourse = "main course"
+    case sideDish = "side dish"
+    case dessert
+    case appetizer
+    case salad
+    case bread
+    case breakfast
+    case soup
+    case beverage
+    case sauce
+    case marinade
+    case fingerfood
+    case snack
+    case drink
+}
