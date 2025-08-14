872a9b7fb808c545acc382019c1fe3787bcd1918
SergeyZakurakin <pricezak@gmail.com>
2025-08-13 22:01:41 -0400

    finish View and ListRow

--- BestRecipes/Modules/SeeAll/SeeAllView.swift
+++ BestRecipes/Modules/SeeAll/SeeAllView.swift
@@ -8,11 +8,30 @@
 import SwiftUI
 
 struct SeeAllView: View {
+    let type: SeeAllExploreType
+    let items: [RecipeModel]
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        List {
+            ForEach(items) { item in
+                RecipeListRow(recipe: item)
+                    .listRowSeparator(.hidden)
+                    .listRowInsets(.init(top: 8, leading: 0, bottom: 16, trailing: 0))
+            }
+        }
+        .listStyle(.plain)
+        .padding(.horizontal, Offsets.x4)
+        .navigationTitle(type.title)
+        .navigationBarTitleDisplayMode(.inline)
+        .background(Color.appBackground.ignoresSafeArea())
     }
 }
 
-#Preview {
-    SeeAllView()
+#Preview("Trending now") {
+    NavigationStack {
+        SeeAllView(type: .trendingNow, items: RecipeModel.trendingMock)
+    }
 }
+
+
+
