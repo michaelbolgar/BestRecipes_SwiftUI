d6a999e719d68c649c17774ecffa235a1f309f67
DmKeller <dmit.keller@gmail.com>
2025-08-16 12:00:42 +0300

    add netService to Home

--- BestRecipes/Modules/Home/HomeViewModel.swift
+++ BestRecipes/Modules/Home/HomeViewModel.swift
@@ -9,16 +9,45 @@ import Foundation
 
 
 final class HomeViewModel: ObservableObject {
+    private let networkService = NetworkingService()
     
     @Published var searchText: String = ""
     @Published var trendingNowRecipes: [RecipeModel] = RecipeModel.trendingMock
     @Published var popularCategoryRecipes: [RecipeModel] = RecipeModel.popularCategoryMock
     @Published var cuisineByCountries: [RecipeModel] = RecipeModel.cuisineByCountryMock
     
+    @Published var currentCategory: String? = nil
+    
+    @Published var error: Error? = nil
     
     // MARK: - Init
     init() {
      
     }
     
+    // MARK: - Fetch Data
+    func fetchTrendingNowRecipes() {
+        do {
+            let result = 
+        } catch {
+            self.error = error.localizedDescription
+        }
+    }
+    
+    func fetchPopularCategoryRecipes() {
+        do {
+            
+        } catch {
+            self.error = error.localizedDescription
+        }
+    }
+    
+    func fetchCuisineByCountries() {
+        do {
+            
+        } catch {
+            self.error = error.localizedDescription
+        }
+    }
+  
 }
