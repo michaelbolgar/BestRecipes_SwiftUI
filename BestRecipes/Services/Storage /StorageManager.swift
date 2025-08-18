2e5acccaa9e03f48f8548dd0cfc493f622156d54
abayellow <sasha.abanshin@gmail.com>
2025-08-18 15:52:00 +1100

    Create storageManager.

--- BestRecipes/Services/Storage /StorageManager.swift
+++ BestRecipes/Services/Storage /StorageManager.swift
@@ -6,3 +6,45 @@
 //
 
 import Foundation
+
+final class StorageManager {
+    static let shared = StorageManager()
+    private let userDefaults = UserDefaults.standard
+    
+
+    private let recipeKey = "recipeKey"
+    private let boolKey = "boolKey"
+    
+    
+    private init() {}
+    
+    // MARK: - Recipe
+    
+    func save(recipe: Recipe) {
+        guard let data = try? JSONEncoder().encode(recipe) else { return }
+        userDefaults.set(data, forKey: recipeKey)
+    }
+    
+    func getRecipe() -> Recipe? {
+        guard let data = userDefaults.data(forKey: recipeKey) else { return nil }
+        return try? JSONDecoder().decode(Recipe.self, from: data)
+    }
+    
+    func deleteRecipe() {
+        userDefaults.removeObject(forKey: recipeKey)
+    }
+    
+    // MARK: - Bool
+    
+    func save(isFirstLaunch: Bool) {
+        userDefaults.set(isFirstLaunch, forKey: boolKey)
+    }
+    
+    func getIsFirstLaunch() -> Bool {
+        return userDefaults.bool(forKey: boolKey)
+    }
+    
+    func deleteIsFirstLaunch() {
+        userDefaults.removeObject(forKey: boolKey)
+    }
+}
