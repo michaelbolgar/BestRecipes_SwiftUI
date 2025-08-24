3658abda9a49b15818c3330bae9b1da23aaf3e15
michaelbolgar <mr.mbolgar@inbox.ru>
2025-08-24 15:16:22 +0200

    added new api key

--- BestRecipes/Services/Networking/API.swift
+++ BestRecipes/Services/Networking/API.swift
@@ -8,7 +8,7 @@
 import Foundation
 
 enum API {
-    static let apiKey = "dbe02e6c827948cab1164a95cab41203"
+    static let apiKey = ApiKeys.secondKey
     static let header = "x-api-key"
 }
 
@@ -16,3 +16,7 @@ enum HTTPMethod: String {
     case get = "GET"
 }
 
+enum ApiKeys: String {
+    case firstKey = "dbe02e6c827948cab1164a95cab41203"
+    case secondKey = "97534030321f4b98b1a71a171f2a5d77"
+}
